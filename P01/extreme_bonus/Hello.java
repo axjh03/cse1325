9f0f0d1b8d5c6a4a6a668d2ea7078a790758561f
Alok Jha <axjh03@gmail.com>
2023-08-25 14:19:13 -0500

    P01 extreme bonus

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+
+public class Hello {
+
+  public static void main(String[] args) {
+
+    String user = System.getProperty("user.name");
+    
+    System.out.println("hello, " + user);
+
+  }
+
+}
