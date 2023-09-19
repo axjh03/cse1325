91c351377490a1dcb681231295bd1dc937b4144f
Alok Jha <axjh03@gmail.com>
2023-09-18 20:09:59 -0500

    Almost done :)

--- P04/extreme_bonus/mdi/LibraryManager.java
+++ P04/extreme_bonus/mdi/LibraryManager.java
@@ -7,16 +7,50 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LibraryManager {
-
     public static void main(String[] args) {
-        Library library = new Library("My Library");
+        Library library = new Library("The Kathmandu Library Lounge (Bagmati)");
+
+        // Load publications from the publications.txt file
         loadPublicationsFromFile(library, "publications.txt");
+
+        // Load patrons from the patrons.txt file
+        loadPatronsFromFile(library, "patrons.txt");
+
+        // Print table
+        System.out.println(library.toString());
+
+        // Input for Selecting Book
+        Scanner input = new Scanner(System.in);
+        int bookChoice = 0, patronChoice = 0;
+
+        try {
+            System.out.print("Which book to check out? ");
+            bookChoice = input.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            System.err.println("Invalid input. Please enter an integer.");
+            System.exit(1); // Exit the program with an error code
+        }
+
+        System.out.println(library.patronMenu());
+
+        try {
+            System.out.print("Who are you? ");
+            patronChoice = input.nextInt();
+        } catch (java.util.InputMismatchException e) {
+            System.err.println("Invalid input. Please enter an integer.");
+            System.exit(1); // Exit the program with an error code
+        }
+
+        // Checkout publication
+        library.checkOut(bookChoice, patronChoice);
+
+        // Print updated table
+        System.out.println(library.toString());
     }
 
+    // Load publications from a text file
     public static void loadPublicationsFromFile(Library library, String fileName) {
-        try {
-            Scanner scanner = new Scanner(new File(fileName));
-
+        try (Scanner scanner = new Scanner(new File(fileName))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] parts = line.split(",");
@@ -36,7 +70,29 @@ public class LibraryManager {
                 }
             }
         } catch (FileNotFoundException e) {
-            System.out.println("Could not load publications file");
+            System.err.println("Could not load publications file: " + fileName);
+            System.exit(1); // Exit the program with an error code
         }
     }
+
+    // Load patrons from a text file
+    // Load patrons from a text file
+    public static void loadPatronsFromFile(Library library, String fileName) {
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+
+                if (parts.length >= 2) {
+                    String name = parts[0];
+                    String email = parts[1];
+                    library.addPatron(new Patron(name, email));
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Could not load patrons file: " + fileName);
+            System.exit(1); // Exit the program with an error code
+        }
+    }
+
 }
