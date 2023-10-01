8c7b278c87190a34ef8ba73beed88020fbf07ed2
Alok Jha <axjh03@gmail.com>
2023-10-01 13:32:43 -0500

    Added some scripts, will delete on final submission

---
+++ P05/extreme_bonus/build.sh
@@ -0,0 +1,8 @@
+# Change to the extreme_bonus directory
+cd /Users/axjh03/Library/CloudStorage/OneDrive-UTArlington/semester/Fall23/CSE1325/github/cse1325/P05/extreme_bonus
+
+# build the application
+ant
+
+# Run the application
+java mdi.LibraryManager library.txt
---
+++ P05/extreme_bonus/clean.sh
@@ -0,0 +1,12 @@
+# Change to the extreme_bonus directory
+cd /Users/axjh03/Library/CloudStorage/OneDrive-UTArlington/semester/Fall23/CSE1325/github/cse1325/P05/extreme_bonus
+
+# go to library and clean
+cd library
+rm *.class
+
+# clean mdi
+cd ..
+cd mdi
+rm *.class
+cd ..
